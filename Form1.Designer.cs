7ce5fd82459324f87c0b67f7473184f3b20316a6
Sompong JongmEEsUK <j_sompong@live.com>
2025-05-09 16:11:09 +0700

    VS2022 SIM900A testing
    9 May 2025

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Sim900A_Rx
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ReceivedBox = new System.Windows.Forms.TextBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // ReceivedBox
+            // 
+            this.ReceivedBox.AllowDrop = true;
+            this.ReceivedBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ReceivedBox.Location = new System.Drawing.Point(330, 38);
+            this.ReceivedBox.Multiline = true;
+            this.ReceivedBox.Name = "ReceivedBox";
+            this.ReceivedBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.ReceivedBox.Size = new System.Drawing.Size(428, 268);
+            this.ReceivedBox.TabIndex = 1;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ReceivedBox);
+            this.Name = "Form1";
+            this.Text = "SIm900A_COM7_Rx";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ReceivedBox;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+
+namespace Sim900A_Rx
+{
+    public partial class Form1 : Form
+    {
+        SerialPort mySerialPort = new SerialPort("COM7", 9600);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_UnLoad(object sender, EventArgs e)
+        {
+           timer.Enabled = false;
+           mySerialPort.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                mySerialPort.Open();
+              //  mySerialPort.Write("RING\r\n");
+                // mySerialPort.WriteLine(message);
+                // mySerialPort.Write(new byte[] { 13 }, 0, 1);
+                // mySerialPort.Write(command);
+                timer.Enabled = true;
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.ToString());
+                //SentBox.Text = ex.ToString();
+                //Console.WriteLine(ex);
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            // mySerialPort.Write("RING\r\n");
+            try
+            {
+                // Open the serial port.
+               // serialPort.Open();
+
+                // Read a line from the serial port.
+                string data = mySerialPort.ReadLine();
+                // ReceivedBox.Text= ("Received data: " + data);
+                ReceivedBox.Text = ReceivedBox.Text + data;
+
+                // Console.WriteLine("Received data: " + data);
+            }
+            catch (Exception ex)
+            {
+               // Console.WriteLine("Error: " + ex.Message);
+                ReceivedBox.Text = ("Error: " + ex.Message);
+            }
+        }
+    }
+}
